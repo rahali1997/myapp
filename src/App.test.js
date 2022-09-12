d3ac6a80af8033d3e77f8f4dd0b465aad1773665
Ahmed Rahali <Ahmed Rahali@cynapsys.local>
2022-09-12 17:01:43 +0100

    test3

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn/i);
+  const linkElement = screen.getByText(/lear/i);
   expect(linkElement).toBeInTheDocument();
 });
